a70e8088c60b939176a9a2960060bec478450d96
Wojciech Gargas <wojciech.gargas@gmail.com>
2025-06-13 08:46:21 +0200

    Add main menu, books database and basic display funtion

--- Biblioteka/main.cpp
+++ Biblioteka/main.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
 #define MAXLEN
 struct book
 {
+	int index;
 	char author[40];
+	char title[40];
+	int numOfPages;
+	int numOfCopies;
 };
 
 
@@ -13,9 +21,21 @@ void displayMainMenu()
 	printf("3.Loogowanie                    4.Dodaj Konto\n");
 	printf("5.Admin                         6.Wyjscie\n");
 }
+void displayBook(struct book Book)
+{
+	printf("Nr  Autor:                    Tytul:                                     Il.Stron:   Ilosc:\n");
+	printf("%2d  %-25s %-40s %10d %8d\n", Book.index, Book.author, Book.title, Book.numOfPages, Book.numOfCopies);
+
+}
 void displayAvaiableBooksMenu()
 {
-	
+	struct book Book;
+	Book.index = 0;
+	strcpy_s(Book.author, 40, "");
+	strcpy_s(Book.title, 40, "");
+	Book.numOfPages = 0;
+	Book.numOfCopies = 0;
+
 	FILE* avaiableBooklist;
 	char posFromList[80];
 	fopen_s(&avaiableBooklist, "avaiableBooks", "r");
@@ -28,8 +48,10 @@ void displayAvaiableBooksMenu()
 	rewind(avaiableBooklist);
 	while (fgets(posFromList, sizeof(posFromList), avaiableBooklist) != NULL)
 	{
-		
+		sscanf_s(posFromList, "%d %s %s %d %d", &Book.index, Book.author, 40, Book.title, 40, &Book.numOfPages, &Book.numOfCopies);
+		displayBook(Book);
 	}
+	fclose(avaiableBooklist);
 
 }
 int main()
@@ -47,7 +69,8 @@ int main()
 	{
 		case 1:
 		{
-
+			displayAvaiableBooksMenu();
+			break;
 		}
 
 	}
