b7dd0919141d3fd4f7de1e883e0cde4f960eb2de
Wojciech Gargas <wojciech.gargas@gmail.com>
2025-06-14 11:01:39 +0200

    Introduced countBooks() and global book count.
    Added saveChangesToArchive() and deductBookFromArchive() to update the archive.
    Rewrote displayAvailableBooks() to show only books in stock.
    Implemented borrowBook() with search by title/author/index and confirmation.
    Replaced file reference to avaiableBooks with consistent Books file.
    Refactored main() into a loop for persistent menu navigation.

--- Biblioteka/main.cpp
+++ Biblioteka/main.cpp
@@ -3,17 +3,77 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define MAXLEN
+#define MAXLEN 40
+#define BOOKSARCHIVE "Books"
 struct book
 {
 	int index;
-	char author[40];
-	char title[40];
+	char author[MAXLEN];
+	char title[MAXLEN];
 	int numOfPages;
 	int numOfCopies;
 };
+int countBooks()
+{ 
+	int numberOfBooksInArchive = 0;
+	int lineIndex;
+	FILE* availableBookList;
+	char line[MAXLEN];
+	fopen_s(&availableBookList, "Books", "r");
+	if (availableBookList == NULL)
+	{
+		puts("Blad odczytu");
+		exit(1);
+	}
+	rewind(availableBookList);
+	while (fscanf_s(availableBookList, "%d %*s %*s %*d %*d", &lineIndex) == 1)
+	{
+		numberOfBooksInArchive++;
+	}
+	fclose(availableBookList);
+	return numberOfBooksInArchive;
+}
 
+int g_numberOfBooksInArchive = countBooks();
 
+void saveChangesToArchive(book Book)
+{
+	int lineFromFile;
+	int offset = 0;
+	int lineIndex = 1;
+	FILE* availableBookList;
+	fopen_s(&availableBookList, "Books", "r+");
+	if (availableBookList == NULL)
+	{
+		puts("Blad odczytu listy ksiazek");
+		exit(1);
+	}
+	
+	rewind(availableBookList);
+	offset = ftell(availableBookList);
+	while(fscanf_s(availableBookList, "%d %*s %*s %*d %*d", &lineFromFile) != 0)
+	{
+		if (lineFromFile == Book.index)
+			break;
+		offset = ftell(availableBookList);
+		lineIndex++;
+	}
+	fseek(availableBookList, offset, SEEK_SET);
+	fprintf(availableBookList, "\n%d %s %s %d %d\n", lineIndex, Book.author, Book.title, Book.numOfPages, Book.numOfCopies);
+	fclose(availableBookList);
+}
+void deductBookFromArchive(book Book, book* Books)
+{
+	for (int i = 0; i < g_numberOfBooksInArchive; i++)
+	{
+		if (memcmp(&Book, &Books[i], sizeof(book)) == 0)
+		{
+			Books[i].numOfCopies--;
+			saveChangesToArchive(Books[i]);
+			break;
+		}
+	}
+}
 void displayMainMenu()
 {
 	printf("         Bibliotek Glowna w Nowym Saczu\n");
@@ -27,55 +87,167 @@ void displayBook(struct book Book)
 	printf("%2d  %-25s %-40s %10d %8d\n", Book.index, Book.author, Book.title, Book.numOfPages, Book.numOfCopies);
 
 }
-void displayAvaiableBooksMenu()
+void displayAvailableBooks()
 {
 	struct book Book;
 	Book.index = 0;
-	strcpy_s(Book.author, 40, "");
-	strcpy_s(Book.title, 40, "");
+	strcpy_s(Book.author, MAXLEN, "");
+	strcpy_s(Book.title, MAXLEN, "");
 	Book.numOfPages = 0;
 	Book.numOfCopies = 0;
 
-	FILE* avaiableBooklist;
+	FILE* availableBookList;
 	char posFromList[80];
-	fopen_s(&avaiableBooklist, "avaiableBooks", "r");
-	if (avaiableBooklist == NULL)
+	fopen_s(&availableBookList, "Books", "r");
+	if (availableBookList == NULL)
 	{
 		puts("Blad odczytu listy ksiazek");
 		exit(1);
 	}
 
-	rewind(avaiableBooklist);
-	while (fgets(posFromList, sizeof(posFromList), avaiableBooklist) != NULL)
+	rewind(availableBookList);
+
+	while (fscanf_s(availableBookList, "%d %s %s %d %d", &Book.index, Book.author, MAXLEN,
+		Book.title, MAXLEN, &Book.numOfPages, &Book.numOfCopies) == 5)
 	{
-		sscanf_s(posFromList, "%d %s %s %d %d", &Book.index, Book.author, 40, Book.title, 40, &Book.numOfPages, &Book.numOfCopies);
-		displayBook(Book);
+		if (Book.numOfCopies > 0)
+			displayBook(Book);
 	}
-	fclose(avaiableBooklist);
+
+	fclose(availableBookList);
 
 }
+void borrowBook()
+{
+	int user_choice;
+	while (1)
+	{
+		printf("           Dostepne ksiazki:\n");
+		displayAvailableBooks();
+		printf("\n\n");
+		printf("1. Wypozycz   2.Powrot\n");
+		while (scanf_s("%d", &user_choice) != 1 && !(user_choice != 1 || user_choice != 2))
+		{
+			puts("Niepoprawne dane, wpisz 1 lub 2");
+			while (getchar() != '\n')
+				continue;
+		}
+		if (user_choice == 1)
+		{
+			char posToFind[MAXLEN];
+			int matchingBookCount = 0;
+			FILE* availableBookList;
+			fopen_s(&availableBookList, "Books", "r");
+
+			book* BooksInLibrary = (book*)malloc(g_numberOfBooksInArchive * sizeof(book));
+			book* BooksMatchingSearch = (book*)malloc(g_numberOfBooksInArchive * sizeof(book));
+
+			for (int i = 0; i < g_numberOfBooksInArchive; i++)
+			{
+				fscanf_s(availableBookList, "%d %s %s %d %d", &BooksInLibrary[i].index, BooksInLibrary[i].author, MAXLEN,
+					BooksInLibrary[i].title, MAXLEN, &BooksInLibrary[i].numOfPages, &BooksInLibrary[i].numOfCopies);
+			}
+
+			fclose(availableBookList);
+
+			puts("Podaj numer, tytul badz autora ksiazki do wypozyczenia");
+			while (scanf_s("%s", posToFind, MAXLEN) && posToFind[0] == '\n')
+			{
+				puts("Podaj numer, tytul badz autora ksiazki do wypozyczenia");
+			}
+
+			for (int i = 0; i < g_numberOfBooksInArchive; i++)
+			{
+				if (strstr(BooksInLibrary[i].author, posToFind) != NULL || strstr(BooksInLibrary[i].title, posToFind) != NULL ||
+					(BooksInLibrary[i].index == atoi(posToFind)))
+				{
+					BooksMatchingSearch[matchingBookCount] = BooksInLibrary[i];
+					matchingBookCount++;
+				}
+			}
+
+			if (matchingBookCount == 0)
+			{
+				puts("Nie znalaziono pozycji odpowiadajacej kryteriom wyszukiwania");
+				continue;
+			}
+			else
+			{
+				puts("Pozycje odpowiadajace kryteriom szukania:");
+			}
+
+			for (int i = 0; i < matchingBookCount; i++)
+			{
+				displayBook(BooksMatchingSearch[i]);
+			}
+
+			if (matchingBookCount == 1)
+			{
+				int borrow = 0;
+				printf("Czy chcesz wypozyczyc ta ksiazke?\n"
+					   "1.Tak        2. Powrot\n");
+				while (scanf_s("%d", &borrow) != 1 && !(borrow != 1 || borrow != 2))
+				{
+					puts("Niepoprawne dane, wybierz 1 lub 2 aby wrocic");
+					while (getchar() != '\n')
+						continue;
+				}
+				if (borrow == 1)
+					deductBookFromArchive(BooksMatchingSearch[0], BooksInLibrary);
+				system("cls");
+			}
+			else
+			{
+
+			}
+
+
+
+
+		}
+		else
+		{
+			system("cls");
+			return;
+		}
+	}
+	
+
+}
+
+
 int main()
 {
 	int mainMenuOption;
-	displayMainMenu();
+	countBooks();
 
-	while (scanf_s("%d", &mainMenuOption) != 1 || mainMenuOption < 1 || mainMenuOption > 6)
+	while (1)
 	{
-		puts("Niepoprawne dane, wpisz ponownie");
-		while (getchar() != '\n')
-			continue;
-	}
-	switch (mainMenuOption)
-	{
+
+		displayMainMenu();
+
+		while (scanf_s("%d", &mainMenuOption) != 1 || mainMenuOption < 1 || mainMenuOption > 6)
+		{
+			puts("Niepoprawne dane, wpisz ponownie");
+			while (getchar() != '\n')
+				continue;
+		}
+		system("cls");
+		switch (mainMenuOption)
+		{
 		case 1:
 		{
-			displayAvaiableBooksMenu();
+			displayAvailableBooks();
 			break;
+		}
+		case 2:
+		{			
+			borrowBook();
+		}
+
 		}
 
 	}
-
-
 
 
 	return 0;
